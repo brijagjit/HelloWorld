38cf5e08be2248403107a9fd2b8949f3fc1d0ade
Brianna Jagjit <brijagjit@gmail.com>
2026-08-08 12:52:56 -0400

    integrating scheduler
    Signed-off-by: Brianna Jagjit <brijagjit@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
-#include <hello.h>
-#include <component1.h>
-#include <component2.h>
-#include <component3.h>
+#include "scheduler.h"
 
 int main()
 {
-    sayHello(Component::SYSTEM);
-    run_component1();
-    run_component2();
-    run_component3();
+   run_scheduler();
 
     return 0;
 }
