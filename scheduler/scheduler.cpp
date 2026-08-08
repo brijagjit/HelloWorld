24eb4578dcc52d35c38c8470eee9d8bf49fd2b23
Brianna Jagjit <brijagjit@gmail.com>
2026-08-08 12:52:56 -0400

    integrating scheduler
    Signed-off-by: Brianna Jagjit <brijagjit@gmail.com>

---
+++ scheduler/scheduler.cpp
@@ -0,0 +1,12 @@
+#include "scheduler.h"
+
+#include "component1.h"
+#include "component2.h"
+#include "component3.h"
+
+void run_scheduler()
+{
+    run_component1();
+    run_component2();
+    run_component3();
+}
