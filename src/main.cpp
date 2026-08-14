46ef758a8ad5b92c5ce19ab1833d19d34ec30af4
Brianna Jagjit <brijagjit@gmail.com>
2026-08-14 12:10:46 -0400

    replacing scheduler with components thread
    Signed-off-by: Brianna Jagjit <brijagjit@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
-#include "scheduler.h"
+#include <functional>
+#include <thread>
+
+#include "component1.h"
+#include "component2.h"
+#include "component3.h"
 
 int main()
 {
-    run_scheduler();
+  int counter = 0;
 
-    return 0;
+  std::thread thread1(run_component1, std::ref(counter));
+  std::thread thread2(run_component2, std::ref(counter));
+  std::thread thread3(run_component3, std::ref(counter));
+
+  thread1.join();
+  thread2.join();
+  thread3.join();
+
+  return 0;
 }
